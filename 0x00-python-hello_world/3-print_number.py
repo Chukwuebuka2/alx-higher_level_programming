052f0dd7f770f62038ea4fa17dfc972b41e82886
EbukVick <55829039+Chukwuebuka2@users.noreply.github.com>
2022-05-30 15:24:05 +0100

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 number = 98
-<<<<<<< HEAD
 print(f"{number} Battery street")
-=======
-print("{} Battery street".format(number))
->>>>>>> 4ac05e21a303fa440f63921402c2f84a1ac6e688
