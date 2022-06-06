bb30bc2683769db7326db59182e70cc53e94e96c
EbukVick <55829039+Chukwuebuka2@users.noreply.github.com>
2022-06-06 16:15:35 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-def  new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
-        if idx == len(my_list):
-            my_list[idx - 1] = element
-            return my_list
-        else:
-            my_list[idx] = element
-            return my_list
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    new_list = my_list[:]
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+    return new_list
 
 
 if __name__ == "__main__":
