8cdca5a1f904af75fad793f6a4d04bd5184b04e4
root <nwokporochukwuebuka@gmail.com>
2022-06-08 23:27:46 -0700

    ndfdg

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-
-<<<<<<< HEAD
-def multiply_by_2(a_dictonary):
-    new_dict = {}
-    for k, v in a_dictionary.items():
-        new_dict[k] = v * 2
-    return new_dict
-=======
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
     list_keys = list(new_dir.keys())
@@ -16,7 +7,6 @@ def multiply_by_2(a_dictionary):
         new_dir[i] *= 2
 
     return (new_dir)
->>>>>>> ce89200cf259a66cac54fec9a2433c09ce3201d0
 
 
 if __name__ == "__main__":
